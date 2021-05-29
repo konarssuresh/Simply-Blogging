07fa2ad1c903b61147f4b155238dfbbeeebda5f8
Suresh Konar <SKonar@evolenthealth.com>
2021-05-29 13:07:49 +0530

    added initial changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,17 @@
-function addBlog(event) {
-  console.log(event);
+const clearBlogForm = () => {
+  document.getElementById("blogImage").value = "";
+  document.getElementById("blogArticle").value = "";
+  document.getElementById("blogTitle").value = "";
+};
+
+const addBlog = (event) => {
   event.preventDefault();
-}
+  const imageUrl = document.getElementById("blogImage").value;
+  const article = document.getElementById("blogArticle").value;
+  const title = document.getElementById("blogTitle").value;
+  console.log({title, article, imageUrl});
+  clearBlogForm();
+};
 
 const form = document.getElementById("addBlogForm");
 form.addEventListener("submit", addBlog);
